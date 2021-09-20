77a3081a2fdf18eaa300e73e12f186dafb7e2f6d
Fabian Kramm <fab.kramm@googlemail.com>
2021-09-20 10:24:17 +0200

    Update devspace_start.sh

--- python/devspace_start.sh
+++ python/devspace_start.sh
@@ -22,7 +22,7 @@ Welcome to your development container!
 This is how you can work with it:
 - Run \`${COLOR_CYAN}python main.py${COLOR_RESET}\` to build the application
 - ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
-- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:3000
+- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:8080
 "
 
 bash
