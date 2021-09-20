880a79a441c661b57cc79e83ba454f7a7ed60a90
Fabian Kramm <fab.kramm@googlemail.com>
2021-09-20 14:58:47 +0200

    Update devspace_start.sh

--- java-maven/devspace_start.sh
+++ java-maven/devspace_start.sh
@@ -21,7 +21,7 @@ This is how you can work with it:
 - Run \`${COLOR_CYAN}mvn package -T 1C -U -Dmaven.test.skip=true${COLOR_RESET}\` to build the application
 - Run \`${COLOR_CYAN}java -jar target/[file].jar${COLOR_RESET}\` to run the application
 - ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
-- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:3000
+- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}http://localhost:8080${COLOR_RESET}
 "
 
 bash
